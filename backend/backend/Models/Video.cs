a9671a01509ed3b23696890eff21d21a370748d2
Aurimas <auruxxas.ar@gmail.com>
2021-03-26 15:30:20 +0200

    Added Backend Context

--- backend/backend/Models/Video.cs
+++ backend/backend/Models/Video.cs
@@ -7,11 +7,16 @@ namespace backend.Models
 {
     public class Video
     {
+        public Video()
+        {
+            UploadDate = DateTime.UtcNow;
+            DeleteDate = null;
+        }
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime UploadDate { get; set; }
-        public DateTime DeleteDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
     }
