020961b4198354d5e80d1534e100124880184cb2
Aurimas <auruxxas.ar@gmail.com>
2021-03-26 16:51:41 +0200

    initial migration to Database

--- backend/backend/Startup.cs
+++ backend/backend/Startup.cs
@@ -26,7 +26,7 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.RegisterDependencies(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
