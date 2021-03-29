d20a37a5621ddbd74d713ecceb21b60c24a0057c
kibartas <juodasis.perlas@gmail.com>
2021-03-29 19:31:13 +0300

    [WDB-50] add redirection; check if user has JWT token in sessionStorage for access to some of the pages; add ConfirmEmailPage to Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import LoginPage from './containers/LoginPage';
 import RegisterPage from './containers/RegisterPage';
+import ConfirmEmailPage from './containers/ConfirmEmailPage';
 import LibraryPage from './containers/LibraryPage';
 import TrashBinPage from './containers/TrashBinPage';
 import ProfilePage from './containers/ProfilePage';
@@ -12,29 +18,47 @@ import ResetPasswordPage from './containers/ResetPasswordPage';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <LibraryPage />
-        </Route>
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <Route exact path="/register">
-          <RegisterPage />
-        </Route>
-        <Route exact path="/bin">
-          <TrashBinPage />
-        </Route>
-        <Route exact path="/profile">
-          <ProfilePage />
-        </Route>
-        <Route exact path="/forgot-password">
-          <ForgotPasswordPage />
-        </Route>
-        <Route exact path="/reset-password">
-          <ResetPasswordPage />
-        </Route>
-      </Switch>
+      {!window.sessionStorage.getItem('token') ? (
+        <Switch>
+          <Route exact path="/">
+            <LoginPage />
+          </Route>
+          <Route exact path="/forgot-password">
+            <ForgotPasswordPage />
+          </Route>
+          <Route exact path="/reset-password">
+            <ResetPasswordPage />
+            <Route exact path="/confirm-email">
+              <ConfirmEmailPage />
+            </Route>
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
+          <Route path="*">
+            <Redirect to="/">
+              <LoginPage />
+            </Redirect>
+          </Route>
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/bin">
+            <TrashBinPage />
+          </Route>
+          <Route exact path="/profile">
+            <ProfilePage />
+          </Route>
+          <Route exact path="/library">
+            <LibraryPage />
+          </Route>
+          <Route path="*">
+            <Redirect to="/library">
+              <LibraryPage />
+            </Redirect>
+          </Route>
+        </Switch>
+      )}
     </Router>
   );
 }
