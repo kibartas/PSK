26b01956487da4a26554882e8e1aca90081327b6
kibartas <juodasis.perlas@gmail.com>
2021-03-29 18:21:45 +0300

    [WDB-50] leave only async/await in the Api examples; change let values to const; add export default to every API file

--- frontend/src/api/UploadAPI.js
+++ frontend/src/api/UploadAPI.js
@@ -1,21 +1,23 @@
-import axios from "axios";
-let baseUri = "https://localhost:44344/api";
+import axios from 'axios';
 
-let Api = axios.create({
-    baseURL: baseUri,
-    headers: {
-        "Content-Type": "multipart/form-data",
-        "Authorization": "Bearer " + window.sessionStorage.getItem("token")
-    }
+const baseUri = 'https://localhost:44344/api';
+
+const Api = axios.create({
+  baseURL: baseUri,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    Authorization: `Bearer ${window.sessionStorage.getItem('token')}`,
+  },
 });
 
-//example file upload
-// export const UploadFiles = async (files) => {
-//     return await Api.post("/Users/upload-files/", files)
-//         .then((response) => {
-//             return response.data;
-//         }).catch(error => {
-//             console.log(error);
-//         })
-// };
+export default Api;
 
+// example file upload
+// export const UploadFiles = async (files) => {
+//     try {
+//         const response = await Api.post("/Users/upload-files/", files);
+//         return response.data;
+//     } catch(error) {
+//         console.log(error);
+//     }
+// };
