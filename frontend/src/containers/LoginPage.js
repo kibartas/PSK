45ef263a25759372077adb941759a77caa7d40a3
kibartas <juodasis.perlas@gmail.com>
2021-03-30 14:45:09 +0300

    [WDB-10] create LoginPage container

--- frontend/src/containers/LoginPage.js
+++ frontend/src/containers/LoginPage.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import LoginForm from '../components/LoginForm/LoginForm';
+import sideImage from '../assets/LoginPage/side-image.svg';
 
 class LoginPage extends React.Component {
   render() {
-    return <h1>Login page</h1>;
+    return (
+      <Grid
+        container
+        justify="space-around"
+        style={{ height: '100vh' }}
+        direction="row"
+        alignItems="center"
+      >
+        <Grid item />
+        <Grid item>
+          <img
+            src={sideImage}
+            alt="Two people looking at their smart devices in front of a video screen"
+          />
+        </Grid>
+        <Grid item>
+          <LoginForm />
+        </Grid>
+        <Grid item />
+      </Grid>
+    );
   }
 }
 
