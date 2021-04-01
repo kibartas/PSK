65dd872c670fc23c71136d21bf351bf8e4fbd808
Vingis Simonas <simonas.vingis@mif.stud.vu.lt>
2021-04-01 17:27:08 +0300

    Added email service

---
+++ backend/backend/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace backend.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly string _weDontByteEmail;
+        private readonly string _weDontBytePassword;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _weDontByteEmail = configuration.GetValue<string>("EmailConfiguration:EmailAddress");
+            _weDontBytePassword = configuration.GetValue<string>("EmailConfiguration:EmailPassword");
+        }
+
+        public void SendVerificationEmail(string receiverName, string receiverEmail, string verificationLink)
+        {
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress("WeDontByte Team", _weDontByteEmail));
+            mailMessage.To.Add(new MailboxAddress(receiverName, receiverEmail));
+            mailMessage.Subject = "Account verification";
+            mailMessage.Body = new TextPart("html")
+            {
+                Text = $"<a href=\"{verificationLink}\">Click here to verify your account!</a>"
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect("smtp.gmail.com", 465, true);
+                smtpClient.Authenticate(_weDontByteEmail, _weDontBytePassword);
+                smtpClient.Send(mailMessage);
+                smtpClient.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ backend/backend/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace backend.Services
+{
+    public interface IEmailService
+    {
+        void SendVerificationEmail(string receiverName, string receiverEmail, string verificationLink);
+    }
+}
