b31433f78b9fe286f70a7d17687ed8bb0442f950
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-04-21 20:45:39 +0300

    [WDB-18] Add registration form

--- frontend/src/containers/RegisterPage/RegisterPage.js
+++ frontend/src/containers/RegisterPage/RegisterPage.js
@@ -1,8 +1,22 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 class RegisterPage extends React.Component {
   render() {
-    return <h1>Register page</h1>;
+    return (
+      <Grid 
+        className="root"
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item xs={10} sm={6} md={4}>
+          <RegistrationForm />
+        </Grid>
+      </Grid>
+    )
   }
 }
 
