322152c9d2e479c84bf0d47c325e6f156f7c51d4
Aurimas <auruxxas.ar@gmail.com>
2021-04-30 17:19:03 +0300

    Register snackbar information

--- frontend/src/containers/RegisterPage/RegisterPage.js
+++ frontend/src/containers/RegisterPage/RegisterPage.js
@@ -1,12 +1,19 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import { register } from '../../api/PublicAPI';
+import CustomSnackbar from '../../components/CustomSnackbar/CustomSnackbar';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 class RegisterPage extends React.Component {
 
-  // [TM]: TODO WDB-28 also delete line below
-  // eslint-disable-next-line no-unused-vars 
+  constructor(props) {
+    super(props);
+    this.state = {
+      showGeneralError: false,
+      showUserExistsError: false
+    };
+  }
+
   handleRegister = (firstName, lastName, email, password) => {
     const data = {
       firstName,
@@ -17,10 +24,25 @@ class RegisterPage extends React.Component {
     register(data).then(() => {
       const { history } = this.props;
       history.push('/confirm-email');
-    });
+    })
+      .catch(ex => {
+        const { status } = ex.response;
+        if (status === 400) this.setState({ showGeneralError: true });
+        else if (status === 409) this.setState({ showUserExistsError: true });
+      })
   }
 
   render() {
+    const { showGeneralError } = this.state;
+    const { showUserExistsError } = this.state;
+
+    const hideGeneralError = () => {
+      this.setState({ showGeneralError: false });
+    }
+
+    const hideUserExistsError = () => {
+      this.setState({ showUserExistsError: false });
+    }
     return (
       <Grid
         container
@@ -29,6 +51,20 @@ class RegisterPage extends React.Component {
         alignItems="center"
         justify="center"
       >
+        {showGeneralError && <Grid item>
+          <CustomSnackbar
+            topCenter
+            message="A server error has occured"
+            onClose={hideGeneralError}
+            severity="error" />
+        </Grid>}
+        {showUserExistsError && <Grid item>
+          <CustomSnackbar
+            topCenter
+            message="User with this email already exists"
+            onClose={hideUserExistsError}
+            severity="error" />
+        </Grid>}
         <Grid item xs={10} sm={6} md={4}>
           <RegistrationForm onRegister={this.handleRegister} />
         </Grid>
