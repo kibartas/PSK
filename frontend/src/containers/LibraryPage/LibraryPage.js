a3b9d7c5fcb88583f119b122f2c9f0d1f7656c51
kibartas <juodasis.perlas@gmail.com>
2021-05-05 00:28:23 +0300

    [WDB-95] create empty LibraryPage and it's styles.css

--- frontend/src/containers/LibraryPage/LibraryPage.js
+++ frontend/src/containers/LibraryPage/LibraryPage.js
@@ -1,12 +1,13 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
-import { UploadIcon } from '../../assets';
+import { UploadIcon, emptyLibraryDrawing } from '../../assets';
 import TopBar from '../../components/TopBar/TopBar';
+import './styles.css';
 
 class LibraryPage extends React.Component {
   render() {
     return (
-      <Grid container>
+      <Grid container direction="column" style={{ height: '100vh' }}>
         <Grid item>
           <TopBar
             title="Video Library"
@@ -23,6 +24,35 @@ class LibraryPage extends React.Component {
               },
             ]}
           />
+        </Grid>
+        <Grid
+          style={{ height: '100vh' }}
+          direction="column"
+          item
+          container
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item>
+            <img
+              id="empty-library"
+              src={emptyLibraryDrawing}
+              alt="Two friendly people standing"
+            />
+          </Grid>
+          <Grid item container direction="column" alignItems="center">
+            <Grid item>
+              <Typography align="center" id="empty-primary-text" variant="h3">
+                It feels empty here...
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography align="center" id="empty-secondary-text" variant="h5">
+                Upload a video by clicking the upload icon on the top
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     );
