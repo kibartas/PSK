c313fb9edcfcf981150e4c98f2e426ec37f19921
kibartas <juodasis.perlas@gmail.com>
2021-05-04 16:15:20 +0300

    [WDB-89] create and finish ResetPasswordCard + page

--- frontend/src/components/ResetPasswordCard/ResetPasswordCard.js
+++ frontend/src/components/ResetPasswordCard/ResetPasswordCard.js
@@ -41,15 +41,16 @@ const ResetPasswordCard = ({ onSend }) => {
 
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
-    setShowPassword(false);
+    setShowConfirmPasswordError(false);
   };
 
-  const handleSendClick = async () => {
+  const handleSendClick = (event) => {
+    event.preventDefault();
     if (!PASSWORD_REGEX.test(password)) {
       setShowPasswordError(true);
       return;
     }
-    if (!PASSWORD_REGEX.test(confirmPassword)) {
+    if (password !== confirmPassword) {
       setShowConfirmPasswordError(true);
       return;
     }
@@ -66,119 +67,123 @@ const ResetPasswordCard = ({ onSend }) => {
         <Typography variant="h4" gutterBottom>
           Reset password
         </Typography>
-        <Grid container spacing={3} justify="center">
+        <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography>Enter a new password for your account</Typography>
-          </Grid>
-          <Grid
-            item
-            container
-            spacing={1}
-            direction="column"
-            alignItems="center"
-          >
             <Grid item xs={12}>
-              <TextField
-                value="F"
-                required
-                error={showPasswordError}
-                helperText={
-                  showPasswordError
-                    ? 'Password must have at least 8 symbols with at least one capital letter and at least one number'
-                    : ''
-                }
-                type={showPassword ? 'text' : 'password'}
-                id="password-field"
-                label="Password"
-                placeholder="**********"
-                variant="outlined"
-                fullWidth
-                onChange={handlePasswordChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleClickShowPassword}>
-                        {showPassword ? (
-                          <img
-                            src={visibilityOffIcon}
-                            alt="Password visibility icon"
-                            width={24}
-                            height={24}
-                          />
-                        ) : (
-                          <img
-                            src={visibilityIcon}
-                            alt="Password visibility off icon"
-                            width={24}
-                            height={24}
-                          />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                value={confirmPassword}
-                required
-                error={showConfirmPasswordError}
-                helperText={
-                  showConfirmPasswordError
-                    ? 'Password must have at least 8 symbols with at least one capital letter and at least one number'
-                    : ''
-                }
-                type={showConfirmPassword ? 'text' : 'password'}
-                id="password-field"
-                label="Confirm password"
-                placeholder="**********"
-                variant="outlined"
-                fullWidth
-                onChange={handleConfirmPasswordChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleClickShowConfirmPassword}>
-                        {showConfirmPassword ? (
-                          <img
-                            src={visibilityOffIcon}
-                            alt="Password visibility icon"
-                            width={24}
-                            height={24}
-                          />
-                        ) : (
-                          <img
-                            src={visibilityIcon}
-                            alt="Password visibility off icon"
-                            width={24}
-                            height={24}
-                          />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
+              <Typography gutterBottom>
+                Enter a new password for your account
+              </Typography>
             </Grid>
           </Grid>
-          <Grid item container direction="row" justify="center" spacing={2}>
-            <Grid item xs={4}>
-              <Button
-                onClick={handleSendClick}
-                fullWidth
-                variant="contained"
-                color="primary"
+          <form noValidate onSubmit={handleSendClick}>
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={10}>
+                <TextField
+                  required
+                  error={showPasswordError}
+                  helperText={
+                    showPasswordError
+                      ? 'Password must have at least 8 symbols with at least one capital letter and at least one number'
+                      : ''
+                  }
+                  type={showPassword ? 'text' : 'password'}
+                  id="password-field"
+                  label="Password"
+                  placeholder="**********"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handlePasswordChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowPassword}>
+                          {showPassword ? (
+                            <img
+                              src={visibilityOffIcon}
+                              alt="Password visibility icon"
+                              width={24}
+                              height={24}
+                            />
+                          ) : (
+                            <img
+                              src={visibilityIcon}
+                              alt="Password visibility off icon"
+                              width={24}
+                              height={24}
+                            />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={10}>
+                <TextField
+                  className="form__last-text-field"
+                  required
+                  error={showConfirmPasswordError}
+                  helperText={
+                    showConfirmPasswordError ? 'Passwords do not match' : ''
+                  }
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  id="confirm-password-field"
+                  label="Confirm Password"
+                  placeholder="**********"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleConfirmPasswordChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowConfirmPassword}>
+                          {showConfirmPassword ? (
+                            <img
+                              src={visibilityOffIcon}
+                              alt="Password visibility icon"
+                              width={24}
+                              height={24}
+                            />
+                          ) : (
+                            <img
+                              src={visibilityIcon}
+                              alt="Password visibility off icon"
+                              width={24}
+                              height={24}
+                            />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid
+                direction="row"
+                wrap="nowrap"
+                item
+                container
+                spacing={2}
+                justify="center"
               >
-                Send
-              </Button>
+                <Grid xs={5} item>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    fullWidth
+                    color="primary"
+                  >
+                    Send
+                  </Button>
+                </Grid>
+                <Grid xs={5} item>
+                  <Button variant="outlined" fullWidth href="/login">
+                    Back to login
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid xs={4} item>
-              <Button variant="outlined" fullWidth href="/login">
-                Back to login
-              </Button>
-            </Grid>
-          </Grid>
+          </form>
         </Grid>
       </CardContent>
     </Paper>
--- frontend/src/containers/ResetPasswordPage/ResetPasswordPage.js
+++ frontend/src/containers/ResetPasswordPage/ResetPasswordPage.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { withRouter } from 'react-router';
+import { Grid } from '@material-ui/core';
+import { resetPassword, verify } from '../../api/PublicAPI';
+import CustomSnackbar from '../../components/CustomSnackbar/CustomSnackbar';
+import ResetPasswordCard from '../../components/ResetPasswordCard/ResetPasswordCard';
 
 class ResetPasswordPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSuccess: false,
+      showVerificationError: false,
+      token: null,
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { token } = match.params;
+    this.setState({ token });
+  }
+
+  onSend = (password) => {
+    const { token } = this.state;
+    resetPassword(token, password)
+      .then(() => this.setState({ showSuccess: true }))
+      .catch(() => this.setState({ showVerificationError: true }));
+  };
+
   render() {
-    return <h1>Reset password page</h1>;
+    const { showSuccess, showVerificationError } = this.state;
+
+    const hideVerificationError = () => {
+      this.setState({ showVerificationError: false });
+    };
+
+    const hideSuccess = () => {
+      this.setState({ showSuccess: false });
+    };
+
+    return (
+      <>
+        {showSuccess && (
+          <CustomSnackbar
+            topCenter
+            message="Your password has been changed successfully"
+            onClose={hideSuccess}
+            severity="success"
+          />
+        )}
+        {showVerificationError && (
+          <CustomSnackbar
+            topCenter
+            message="A server error has occurred. Please try later"
+            onClose={hideVerificationError}
+            severity="error"
+          />
+        )}
+        <Grid
+          container
+          className="root"
+          direction="column"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item xs={10} sm={6} md={4} lg={3}>
+            <ResetPasswordCard onSend={this.onSend} />
+          </Grid>
+        </Grid>
+      </>
+    );
   }
 }
 
-export default ResetPasswordPage;
+export default withRouter(ResetPasswordPage);
