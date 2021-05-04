dc1079a35d905763fcbd1030cf30ad6dd30a13a5
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-05-04 19:11:14 +0300

    [WDB-32] Add required assets, add default fill color for icons

--- frontend/src/assets/generic/DownloadIcon.js
+++ frontend/src/assets/generic/DownloadIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { GRAY_1 } from '../../constants';
 
-const DownloadIcon = ({ fill, width = '24px', height = '24px' }) => (
+const DownloadIcon = ({ fill = {GRAY_1}, width = '24px', height = '24px' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
