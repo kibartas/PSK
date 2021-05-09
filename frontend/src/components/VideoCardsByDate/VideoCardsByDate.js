1644f1db77e120008ace767347e148c952cd427d
kibartas <juodasis.perlas@gmail.com>
2021-05-09 21:06:27 +0300

    [WDB-60] create VideoCard and VideoCardsByDate containers

---
+++ frontend/src/components/VideoCardsByDate/VideoCardsByDate.js
@@ -0,0 +1,26 @@
+import { Divider, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import VideoCard from '../VideoCard/VideoCard';
+
+const VideoCardsByDate = ({ videoCards, onSelect }) => (
+  <Grid container item direction="column">
+    <Grid item>
+      <Typography variant="h6">{videoCards[0].date}</Typography>
+      <Divider />
+    </Grid>
+    <Grid container item direction="row" spacing={5} style={{ marginTop: 0 }}>
+      {videoCards.map(({ title, thumbnail, id }) => (
+        <Grid item key={id}>
+          <VideoCard
+            id={id}
+            title={title}
+            onSelect={onSelect}
+            thumbnail={thumbnail}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  </Grid>
+);
+
+export default VideoCardsByDate;
