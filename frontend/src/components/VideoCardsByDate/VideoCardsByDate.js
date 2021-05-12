24b990a3ee05661f505c89c387be1a738e79881d
kibartas <juodasis.perlas@gmail.com>
2021-05-12 14:41:59 +0300

    [WDB-60] fix styling and naming and stuff

--- frontend/src/components/VideoCardsByDate/VideoCardsByDate.js
+++ frontend/src/components/VideoCardsByDate/VideoCardsByDate.js
@@ -1,16 +1,17 @@
 import { Divider, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import VideoCard from '../VideoCard/VideoCard';
+import './styles.css';
 
 const VideoCardsByDate = ({ videoCards, onSelect, selectedCards }) => (
   <Grid container item direction="column">
     <Grid item>
-      <Typography variant="h6">{videoCards[0].date}</Typography>
+      <Typography variant="h6">{videoCards[0].uploadDate}</Typography>
       <Divider />
     </Grid>
     <Grid container item direction="row" spacing={5} style={{ marginTop: 0 }}>
       {videoCards.map(({ title, thumbnail, id }) => (
-        <Grid item key={id}>
+        <Grid item className="card_grid" key={id}>
           <VideoCard
             id={id}
             title={title}
