5d94d9c6b75cb3601cbf187da607668a802c6393
kibartas <juodasis.perlas@gmail.com>
2021-05-21 01:09:45 +0300

    change API url

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const baseUri = 'http://localhost:61346/api';
+let baseUri;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  baseUri = 'http://localhost:61346/api';
+} else {
+  baseUri = 'http://13.81.9.144/api';
+}
 
 const Api = axios.create({
   baseURL: baseUri,
