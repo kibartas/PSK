e9fe7b0292e43f73425c52efaf1d162808866fff
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-05-26 20:29:36 +0300

    Parse some video info from video html tag

--- frontend/src/containers/PlayerPage/PlayerPage.js
+++ frontend/src/containers/PlayerPage/PlayerPage.js
@@ -31,6 +31,8 @@ class PlayerPage extends React.Component {
     this.state = {
       url: undefined,
       video: undefined,
+      resolution: undefined,
+      duration: undefined,
       screenWidth: window.innerWidth,
       screenHeight: window.innerHeight,
       showPlaybackError: false,
@@ -46,6 +48,7 @@ class PlayerPage extends React.Component {
       oldTitle: '',
       informationDrawerTitle: '',
     };
+    this.setResolutionAndDuration = this.setResolutionAndDuration.bind(this);
     this.topBarRef = React.createRef();
   }
 
@@ -58,33 +61,42 @@ class PlayerPage extends React.Component {
       .then((response) => {
         const token = localStorage.getItem('token');
         const url = `http://localhost:61346/api/Videos/stream?videoId=${videoId}&token=${token}`;
-
         const video = this.transformVideo(response.data);
-        this.setState({ url, video, informationDrawerTitle: video.title });
+        this.setState({url, video, informationDrawerTitle: video.title});
+
+        const videoElement = document.getElementsByTagName("video")[0];
+        videoElement.addEventListener("loadedmetadata", this.setResolutionAndDuration(videoElement));
       })
       .catch(() => this.setState({ showPlaybackError: true }));
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateWindowDimensions);
+    const videoElement = document.getElementsByTagName("video")[0];
+    videoElement.removeEventListener("loadedmetadata", this.setResolutionAndDuration(videoElement));
   }
 
   transformVideo = (source) => {
     const size = formatBytesToString(source.size);
-    const resolution = `${source.width}x${source.height}`;
-    const duration = secondsToHms(source.duration);
     const isFromBin = !!source?.deleteDate;
     return {
       id: source.id,
       title: source.title,
       format: source.format,
-      duration,
-      resolution,
       size,
       isFromBin,
       rowVersion: source.rowVersion,
     };
   };
+
+  setResolutionAndDuration = (videoElement) => () => {
+    const resolution = `${videoElement.videoWidth}x${videoElement.videoHeight}`;
+    const duration = secondsToHms(videoElement.duration);
+    this.setState({
+      resolution,
+      duration,
+    });
+  }
 
   updateWindowDimensions = () =>
     this.setState({
@@ -258,6 +270,8 @@ class PlayerPage extends React.Component {
     const {
       url,
       video,
+      resolution,
+      duration,
       screenWidth,
       screenHeight,
       showPlaybackError,
@@ -270,6 +284,9 @@ class PlayerPage extends React.Component {
       oldTitle,
       informationDrawerTitle,
     } = this.state;
+
+    const isFromBin = video ? video.isFromBin : false;
+    const shouldRenderInfoDrawer = video && duration && resolution;
 
     // This fallback height is needed, since TopBar is not rendered until video information is fetched, so ref will be null
     const fallBackTopBarHeight = screenWidth > 600 ? 64 : 56; // These values are from Material UI AppBar source code
@@ -287,109 +304,106 @@ class PlayerPage extends React.Component {
           onCancel={this.handleOverwriteTitleDialogCancel}
           onConfirm={this.handleOverwriteTitleDialogConfirm}
         />
-        {video === undefined ? (
-          showPlaybackError && (
+        {showPlaybackError && (
             <CustomSnackbar
               topCenter
               message="A playback error has occurred. Please try again later"
               onClose={this.hidePlaybackError}
               severity="error"
             />
-          )
-        ) : (
-          <>
-            {this.renderDownloadSnackbars()}
-            <DeleteConfirmationDialog
-              open={showDeletionDialog}
-              onConfirm={
-                video.isFromBin
-                  ? this.handleVideoDeletion
-                  : this.handleVideoMarkForDeletion
-              }
-              onCancel={this.toggleDeletionDialog}
-              deleteForever={video.isFromBin}
-            />
-            {showDeletionError && (
-              <CustomSnackbar
-                message="Oops... Something wrong happened, we could not delete your video"
-                onClose={this.hideDeletionError}
-                severity="error"
-              />
-            )}
-            {showVideoRestorationError && (
-              <CustomSnackbar
-                message="Oops... Something wrong happened, we could not restore your video"
-                onClose={this.hideDeletionError}
-                severity="error"
-              />
-            )}
-            {showChangeTitleError && (
-              <CustomSnackbar
-                message="Oops... Something wrong happened, we could not restore your video"
-                onClose={this.hideDeletionError}
-                severity="error"
-              />
-            )}
-            <div ref={this.topBarRef}>
-              <TopBar
-                darkMode
-                firstName={window.localStorage.getItem('firstName')}
-                lastName={window.localStorage.getItem('lastName')}
-                title={video.title}
-                showArrow
-                onIconsClick={
-                  !video.isFromBin
-                    ? [
-                        this.toggleInformationDrawer,
-                        this.handleVideoDownload,
-                        this.toggleDeletionDialog,
-                      ]
-                    : [
-                        this.toggleInformationDrawer,
-                        this.handleRestore,
-                        this.toggleDeletionDialog,
-                      ]
-                }
-                onActionIconClick={this.handleArrowBackClick}
-                iconsToShow={
-                  !video.isFromBin
-                    ? [InfoIcon, DownloadIcon, DeleteIcon]
-                    : [InfoIcon, RestoreIcon, DeleteForeverIcon]
-                }
-              />
-            </div>
-            <InformationDrawer
-              open={showInformationDrawer}
-              onOpen={this.toggleInformationDrawer}
-              onClose={this.toggleInformationDrawer}
-              title={informationDrawerTitle}
-              setTitle={this.handleInformationDrawerTitleChange}
-              videoDuration={video.duration}
-              videoSize={video.size}
-              videoFormat={video.format}
-              videoResolution={video.resolution}
-              onVideoTitleChange={this.handleVideoTitleChange}
-              disableTextField={showChangeTitleConflict}
-            />
-            <div className="player-wrapper">
-              <ReactPlayer
-                playing
-                width={screenWidth}
-                height={screenHeight - topBarHeight}
-                url={url}
-                controls
-                config={{
-                  file: {
-                    attributes: {
-                      onContextMenu: (e) => e.preventDefault(),
-                      controlsList: 'nodownload',
-                    },
-                  },
-                }}
-              />
-            </div>
-          </>
         )}
+        {this.renderDownloadSnackbars()}
+        <DeleteConfirmationDialog
+          open={showDeletionDialog}
+          onConfirm={
+            isFromBin
+              ? this.handleVideoDeletion
+              : this.handleVideoMarkForDeletion
+          }
+          onCancel={this.toggleDeletionDialog}
+          deleteForever={isFromBin}
+        />
+        {showDeletionError && (
+          <CustomSnackbar
+            message="Oops... Something wrong happened, we could not delete your video"
+            onClose={this.hideDeletionError}
+            severity="error"
+          />
+        )}
+        {showVideoRestorationError && (
+          <CustomSnackbar
+            message="Oops... Something wrong happened, we could not restore your video"
+            onClose={this.hideDeletionError}
+            severity="error"
+          />
+        )}
+        {showChangeTitleError && (
+          <CustomSnackbar
+            message="Oops... Something wrong happened, we could not restore your video"
+            onClose={this.hideDeletionError}
+            severity="error"
+          />
+        )}
+        <div ref={this.topBarRef}>
+          <TopBar
+            darkMode
+            firstName={window.localStorage.getItem('firstName')}
+            lastName={window.localStorage.getItem('lastName')}
+            title={video ? video.title : ''}
+            showArrow
+            onIconsClick={
+              !isFromBin
+                ? [
+                    this.toggleInformationDrawer,
+                    this.handleVideoDownload,
+                    this.toggleDeletionDialog,
+                  ]
+                : [
+                    this.toggleInformationDrawer,
+                    this.handleRestore,
+                    this.toggleDeletionDialog,
+                  ]
+            }
+            onActionIconClick={this.handleArrowBackClick}
+            iconsToShow={
+              !isFromBin
+                ? [InfoIcon, DownloadIcon, DeleteIcon]
+                : [InfoIcon, RestoreIcon, DeleteForeverIcon]
+            }
+          />
+        </div>
+        {shouldRenderInfoDrawer && (
+          <InformationDrawer
+            open={showInformationDrawer}
+            onOpen={this.toggleInformationDrawer}
+            onClose={this.toggleInformationDrawer}
+            title={informationDrawerTitle}
+            setTitle={this.handleInformationDrawerTitleChange}
+            videoDuration={duration}
+            videoSize={video.size}
+            videoFormat={video.format}
+            videoResolution={resolution}
+            onVideoTitleChange={this.handleVideoTitleChange}
+            disableTextField={showChangeTitleConflict}
+          />
+        )}
+        <div className="player-wrapper">
+          <ReactPlayer
+            playing
+            width={screenWidth}
+            height={screenHeight - topBarHeight}
+            url={url}
+            controls
+            config={{
+              file: {
+                attributes: {
+                  onContextMenu: (e) => e.preventDefault(),
+                  controlsList: 'nodownload',
+                },
+              },
+            }}
+          />
+        </div>
       </div>
     );
   }
